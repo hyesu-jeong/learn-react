9d2b45eff41c352ceb8a046a347c7e0d115bb2a3
hyesu-jeong <hyeppy739@gmail.com>
2024-07-25 14:39:15 +0900

    [학습] 리액트 앱 렌더링
    - React 엘리먼트 생성 및 UI 트리 구성
    - ReactDOM 브라우저 화면에 렌더링

--- 04-getting-started-with-react/src/virtual-dom-vs-actual-dom.js
+++ 04-getting-started-with-react/src/virtual-dom-vs-actual-dom.js
@@ -3,48 +3,49 @@
 // 실제 DOM을 추상화(단순화)
 
 // virtual
-import { createElement } from "./lib/virtual/index.js";
+import { createElement } from './lib/virtual/index.js';
 // virtual-dom
-import { createRoot } from "./lib/virtual-dom/index.js";
+import { createRoot } from './lib/virtual-dom/index.js';
 
-// 실제 DOM에 <figure></figure> 요소를 추가
-// 웹 API를 사용하여 문서 객체(Document Object) 생성
+// -----------------------------------------------------------------
 
-// 부모(상위) 요소
-const figureElement = document.createElement("figure");
-console.dir(figureElement);
-// => 실제 돔 객체는 복잡하다.
+// 가상(추상화된, 단순화된) 요소(엘리먼트) 생성
 
 // 자식(하위) 요소
-const figcaptionElement = document.createElement("figcaption");
+const figcaptionVElement = createElement('figcaption');
+// console.log(figcaptionVElement);
+
+// 부모(상위) 요소
+// API : createElement(type, props, child1, child2, ..., childN)
+// API : createElement(type, props, ...children)
+const figureVElement = createElement('figure', null, figcaptionVElement);
+// console.log(figureVElement);
+
+// virtual-dom / createRoot
+// 가상 요소를 실제 DOM 요소로 렌더링
+
+// API : createRoot(container)
+const virtualRootElement = document.getElementById('virtual-dom');
+const vRoot = createRoot(virtualRootElement);
+vRoot.render(figureVElement);
+
+// -----------------------------------------------------------------
+
+// 실제 DOM 구성 (엘리먼트 트리)
+// 웹 API를 사용해 문서 객체(Document Object) 생성
+// <figure> 요소를 생성하고 싶어요.
+
+// 부모(상위) 요소 생성
+const figureElement = document.createElement('figure');
+
+// 자식(하위) 요소 생성
+const figcaptionElement = document.createElement('figcaption');
 
 // 요소간 관계 형성
-figcaptionElement.textContent = "hello";
 figureElement.append(figcaptionElement);
 
 // 실제 DOM에 마운트(mount, 착장 === 렌더링)
-const actualDomElement = document.getElementById("actual-dom");
+const actualDomElement = document.getElementById('actual-dom');
 console.log(actualDomElement);
 
 actualDomElement.append(figureElement);
-
-// -----------------------------------------------------------------------
-
-/// 가상(추산화된, 단순화된) 요소(엘리먼트) 생성
-
-// 자식(하위) 요소
-const figcaptionVElement = createElement("figcaption");
-
-// 부모(상위) 요소
-const figureVElement = createElement("figure", null, figcaptionVElement);
-console.dir(figureVElement);
-
-// virtual-dom / createRoot
-// 가상 요소를 실제  DOM 요소로 렌더링
-
-// API : createRoot(container)
-const virtualRootElement = document.getElementById("virtual-dom");
-const vRoot = createRoot(virtualRootElement);
-console.log(vRoot.render);
-
-vRoot.render(figureVElement);
