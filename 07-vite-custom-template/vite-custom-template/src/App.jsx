ba11bab3ea2b0a1398713d122c0b0d76e906f8c1
hyesu-jeong <hyeppy739@gmail.com>
2024-07-29 13:37:53 +0900

    [환경구성] React 자동주입 플러그인 설치
    - @vitejs/plugin-react 설치
    - Vite 구성 파일 생성 후, 플러그인 연결

---
+++ 07-vite-custom-template/vite-custom-template/src/App.jsx
@@ -0,0 +1,16 @@
+// React is not defined
+// import React from "react";
+
+// JSX Runtime : Classic
+// JSX Runtime : Automatic, new JSX Transform (React v17+)
+// current React Version : v18, v19 RC
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React 웹 앱</h1>
+    </div>
+  );
+}
+
+export default App;
