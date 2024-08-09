2eb354fcb50982ae0f65294b5beeae8638164995
hyesu-jeong <hyeppy739@gmail.com>
2024-08-09 16:18:40 +0900

    [학습] 틱택토 게임 만들기 Part 5
    - 게임 상태 메시지 표시
    - 상태 끌어올리기 (Board → Game) 이유 : History 컴포넌트와 상태 공유

--- 13-tic-tac-toe-game/src/tic-tac-toe/components/Status/Status.jsx
+++ 13-tic-tac-toe-game/src/tic-tac-toe/components/Status/Status.jsx
@@ -1,14 +1,26 @@
+// --------------------------------------------------------------------------
+// ✅ 틱택토 게임 만들기 (Status 컴포넌트)
+// --------------------------------------------------------------------------
+// - [ ]
+// --------------------------------------------------------------------------
+
+import { OneOfPlayerType } from '@/tic-tac-toe/types/type.d';
 import S from './Status.module.css';
+import { bool } from 'prop-types';
 
-{
-  /* <h2>승자! : 🟨</h2> */
-}
-{
-  /* <h2>비겼어요... 😳</h2> */
-}
+Status.propTypes = {
+  winner: OneOfPlayerType,
+  nextPlayer: OneOfPlayerType.isRequired,
+  isDraw: bool,
+};
 
-function Status() {
-  return <h2 className={S.component}>플레이어 : 🟨</h2>;
+function Status({ winner, nextPlayer, isDraw = false }) {
+  // 기본적인 메시지는 다음 플레이어 표시
+  let statusMessage = `다음 플레이어 : ${nextPlayer}`;
+  if (winner) statusMessage = `위너!! ${winner}`;
+  if (isDraw) statusMessage = '음... 비겼네. 😩 한 판 더?!';
+
+  return <h2 className={S.component}>{statusMessage}</h2>;
 }
 
 export default Status;
