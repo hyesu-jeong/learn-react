9b7b77ff772778cd8323e42f91d145354d2e9c6e
hyesu-jeong <hyeppy739@gmail.com>
2024-08-12 13:18:14 +0900

    [학습] 홈 페이지 만들기 - React SPA (Single Page Application) - 루트(route)에 따라 어떤 컴포넌트를 조건부 렌더링 - 루트 객체 설정

--- 14-using-react-refs/src/routes/index.jsx
+++ 14-using-react-refs/src/routes/index.jsx
@@ -5,8 +5,14 @@ import MotionOneTimeline from './motion-one-timeline';
 import MotionOneInView from './motion-one-in-view';
 import MotionOneScroll from './motion-one-in-scroll';
 import MotionOneStagger from './motion-one-stagger';
+import Home from './home/home';
 
 const routes = [
+  {
+    title: '리액트로 만드는 싱글 페이지 앱',
+    path: '/',
+    element: <Home />,
+  },
   {
     title: '리-렌더 없이 기억',
     path: '/remember-without-re-render',
