b9c40fd466d25dbe431c98ed4b9f54b1a6e1343c
hyesu-jeong <hyeppy739@gmail.com>
2024-08-13 11:35:19 +0900

    [학습] 타임라인 애니메이션 I - 멀티 요소에 animate() 함수 적용

--- 14-using-react-refs/src/routes/motion-one-timeline/components/SVGPathTimeline.jsx
+++ 14-using-react-refs/src/routes/motion-one-timeline/components/SVGPathTimeline.jsx
@@ -10,13 +10,35 @@
 
 import { number } from 'prop-types';
 import S from './SVGPathTimeline.module.css';
+import { useRef } from 'react';
+import { animate } from 'motion';
 
 SVGPathTimeline.propTypes = {
   size: number,
 };
 
 function SVGPathTimeline({ size = 60 }) {
-  const handleTimelineAnimate = () => {};
+  const svgCircleRef = useRef(null);
+  const svgPathRef = useRef(null);
+
+  const handleTimelineAnimate = () => {
+    const { current: circleElement } = svgCircleRef;
+    // const circleElement = svgCircleRef.current;
+    const { current: pathElement } = svgPathRef;
+    // const pathElement = svgPathRef.current;
+
+    animate(
+      circleElement,
+      { strokeDashoffset: [1, 0], visibility: 'visible' },
+      { duration: 0.8, easing: 'ease-in-out' }
+    );
+
+    animate(
+      pathElement,
+      { strokeDashoffset: [1, 0], visibility: 'visible' },
+      { duration: 0.6, easing: 'ease-in-out', delay: 0.6 }
+    );
+  };
 
   return (
     <div className={S.component}>
@@ -29,8 +51,16 @@ function SVGPathTimeline({ size = 60 }) {
       </button>
 
       <svg width={size} height={size} viewBox="0 0 200 200">
-        <circle className={S.circle} cx="100" cy="100" r="80" pathLength="1" />
+        <circle
+          ref={svgCircleRef}
+          className={S.circle}
+          cx="100"
+          cy="100"
+          r="80"
+          pathLength="1"
+        />
         <path
+          ref={svgPathRef}
           className={S.path}
           d="M 54 107.5 L 88 138.5 L 144.5 67.5"
           pathLength="1"
