84b5a96ab3e15cde986e54059407447b2b645e77
hyesu-jeong <hyeppy739@gmail.com>
2024-08-14 16:14:35 +0900

    [학습] 이펙트 활용 (웹 스토리지 동기화) - 로컬 스토리지에 데이터 쓰기 - 로컬 스토리지에 데이터 읽기 - 상태 초기화 함수가 필요한 이유 - 이펙트 vs. 이벤트 핸들러

--- 15-effect-sync-external-system/src/main.jsx
+++ 15-effect-sync-external-system/src/main.jsx
@@ -1,4 +1,5 @@
 import '@/styles/main.css';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
@@ -8,8 +9,8 @@ if (!container) {
   console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
 } else {
   createRoot(container).render(
-    <>
+    <StrictMode>
       <App />
-    </>
+    </StrictMode>
   );
 }
