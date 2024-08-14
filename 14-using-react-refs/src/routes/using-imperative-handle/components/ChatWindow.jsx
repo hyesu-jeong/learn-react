49e00baf4f4f44a838bc9e04fab013f9d3068ded
hyesu-jeong <hyeppy739@gmail.com>
2024-08-14 09:48:10 +0900

    [학습] 채팅 윈도우 구현 I - 컴포넌트 간 상태 공유 및 상태 업데이트

--- 14-using-react-refs/src/routes/using-imperative-handle/components/ChatWindow.jsx
+++ 14-using-react-refs/src/routes/using-imperative-handle/components/ChatWindow.jsx
@@ -1,46 +1,62 @@
-import { useId } from 'react';
+import { arrayOf, bool, exact, func, string } from 'prop-types';
+import { useId, useRef } from 'react';
 import S from './ChatWindow.module.css';
 
-function ChatWindow() {
+const MessageType = exact({
+  id: string.isRequired,
+  message: string.isRequired,
+  isMe: bool.isRequired,
+});
+
+const MessageListType = arrayOf(MessageType);
+
+ChatWindow.propTypes = {
+  messages: MessageListType.isRequired,
+  onAddMessage: func,
+};
+
+function ChatWindow({ messages, onAddMessage }) {
   const id = useId();
+  const textareaRef = useRef(null);
+
+  const handleSendMessage = (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.currentTarget);
+
+    let newMessage = formData.get('message');
+    newMessage = newMessage.trim();
+
+    if (newMessage.length <= 0) {
+      alert('메시지 내용을 입력하세요!');
+      textareaRef.current.select();
+    }
+
+    onAddMessage?.(newMessage);
+  };
 
   return (
     <section className={S.component}>
       <h2 className="sr-only">채팅 화면</h2>
 
       <ol className={S.chats}>
-        <li className={S.chat}>
-          오늘 저녁에 뭐 먹을까?
-        </li>
-        <li className={`${S.chat} ${S.me}`}>
-          오늘? 저녁에? 뭐 먹지?
-        </li>
-        <li className={S.chat}>
-          치킨 먹을까? 🐓 🍗 어때?
-        </li>
-        <li className={`${S.chat} ${S.me}`}>
-          치킨? 뭐 매일 치킨이야? 난 싫어.
-        </li>
-        <li className={S.chat}>
-          그럼 짜장에 탕수육 어때?
-        </li>
-        <li className={`${S.chat} ${S.me}`}>
-          별로... 하아 좀 맛있는 거 없나? 그냥 회 먹자. 알았지?
-        </li>
-        <li className={S.chat}>
-          음.... 그래. 알았어.
-        </li>
-        <li className={`${S.chat} ${S.me}`}>
-          좋아! 좀 있다 사가정역 2번 출구에서 만나~ 😃 
-        </li>
+        {messages.map(({ id, isMe, message }) => {
+          const classNames = `${S.chat} ${isMe ? S.me : ''}`.trim();
+
+          return (
+            <li key={id} className={classNames}>
+              {message}
+            </li>
+          );
+        })}
       </ol>
 
-      <form className={S.form}>
+      <form className={S.form} onSubmit={handleSendMessage}>
         <div className={S.messageBox}>
           <label htmlFor={id} className="sr-only">
             메시지 입력
           </label>
-          <textarea id={id} />
+          <textarea ref={textareaRef} id={id} name="message" />
         </div>
         <button type="submit">보내기</button>
       </form>
