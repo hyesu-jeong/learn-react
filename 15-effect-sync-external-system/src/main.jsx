09f4872463e9d92bbf97a0837f7d19d9d8586d00
hyesu-jeong <hyeppy739@gmail.com>
2024-08-14 14:45:16 +0900

    [학습] 이펙트 활용 (문서 제목 동기화) - 리액트 외부 시스템과 동기화 - 브라우저의 문서 제목 동기화 (반응성 데이터 변경) - 이펙트 추가 (useEffect 훅) - 이펙트 콜백 함수 설정 - 종속성(의존성) 설정 (배열에 반응성 데이터 추가)

--- 15-effect-sync-external-system/src/main.jsx
+++ 15-effect-sync-external-system/src/main.jsx
@@ -1,6 +1,5 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
 import '@/styles/main.css';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('react-app');
@@ -9,8 +8,8 @@ if (!container) {
   console.warn('문서에 "#app" 요소가 존재하지 않습니다.');
 } else {
   createRoot(container).render(
-    <StrictMode>
+    <>
       <App />
-    </StrictMode>
+    </>
   );
 }
