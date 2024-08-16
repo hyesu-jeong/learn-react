be48ec436457a63bf355b3b8c0f53e3c0994e276
hyesu-jeong <hyeppy739@gmail.com>
2024-08-16 11:46:35 +0900

    [학습] 스크롤 트리거 이펙트 I - getRandomMinMax 유틸리티 함수

--- 15-effect-sync-external-system/src/routes/scroll-trigger-effect/components/Peekaboo.jsx
+++ 15-effect-sync-external-system/src/routes/scroll-trigger-effect/components/Peekaboo.jsx
@@ -1,27 +1,30 @@
 // --------------------------------------------------------------------------
 // ✅ 스크롤 트리거 이펙트
 // --------------------------------------------------------------------------
-// - [ ] 코드 해설
+// - [x] 코드 해설
 // - [ ] sectionMapRef 참조 객체에 순환된 <section> 요소를 Map 데이터로 수집합니다.
 // - [ ] 랜덤 <section> 요소가 뷰포트 안에 들어왔는 지 확인합니다. (IntersectionObserver)
 // - [ ] 랜덤 <section> 요소가 뷰포트 안에 있으면 피카부(까꿍) 애니메이션 발동되도록 구현합니다.
 // --------------------------------------------------------------------------
 
-import { useEffect, useRef, useState } from 'react';
+import { getRandomMinMax } from '@/utils';
 import { animate, spring } from 'motion';
+import { useEffect, useRef, useState } from 'react';
 import S from './Peekaboo.module.css';
 
 function Peekaboo() {
   const [sections] = useState(Array(9).fill(null));
 
-  const [peekaboo] = useState(false);
+  // 스크롤 영역에 랜덤 인덱스의 섹션이 보이면 까꿍~!
+  // peekaboo 상태가 true가 되면 유령이 등장!
+  const [peekaboo] = useState(true);
 
   const [randomIndex] = useState(() => {
     const min = 4;
-    const max = sections.length;
-    const randomIndex = Math.round(Math.random() * (max - min) + min);
+    const max = sections.length; // 9
+    const randomIndex = getRandomMinMax(min, max);
     console.log(randomIndex);
-    return 1;
+    return randomIndex;
   });
 
   const peekabooRef = useRef(null);
@@ -39,14 +42,14 @@ function Peekaboo() {
     if (peekaboo) {
       animate(
         peekabooCharacter,
-        { x: [1000, 0], opacity: 1 },
+        { x: [1000, 0], opacity: [0, 1] },
         {
           delay: 0.5,
           easing: spring({ stiffness: 800, damping: 15, mass: 2 }),
         }
       );
     } else {
-      animate(peekabooCharacter, { x: [0, 1000], opacity: 0 });
+      animate(peekabooCharacter, { x: [0, 1000], opacity: [0, 1] });
     }
   }, [peekaboo]);
 
--- 15-effect-sync-external-system/src/utils/index.js
+++ 15-effect-sync-external-system/src/utils/index.js
@@ -1,3 +1,4 @@
 export * from './debounce';
+export * from './getRandomMinMax';
 export * from './throttle';
 export * from './web-storage';
