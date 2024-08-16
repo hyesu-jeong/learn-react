cefd8daecb1239c848bd961a1afa2eb54cad68e7
hyesu-jeong <hyeppy739@gmail.com>
2024-08-16 10:24:08 +0900

    [학습] 이펙트 동기화 / 정리 II - 잦은 이벤트 발생에 따른 성능 이슈 - 디바운싱, 쓰로틀링을 활용해서 성능 이슈 완화

--- 15-effect-sync-external-system/src/routes/effect-sync-and-cleanup/components/PrintMousePosition.jsx
+++ 15-effect-sync-external-system/src/routes/effect-sync-and-cleanup/components/PrintMousePosition.jsx
@@ -5,6 +5,7 @@
 // - [ ] 컴포넌트가 언마운트 된 이후 남은 이펙트를 깨끗하게 정리합니다.
 // --------------------------------------------------------------------------
 
+import { throttle } from '@/utils';
 import { useEffect, useState } from 'react';
 import S from './PrintMousePosition.module.css';
 
@@ -22,6 +23,7 @@ function PrintMousePosition() {
   useEffect(
     // 1. 이펙트 콜백 추가
     () => {
+      console.log('이벤트 연결');
       // [이벤트 연결]
       // 사용자가 화면에서 마우스를 움직이면 이를 추적해
       // 마우스의 x, y 좌표 값을 읽어온다.
@@ -30,12 +32,13 @@ function PrintMousePosition() {
         setMousePosition({ x, y });
       };
 
-      document.addEventListener('mousemove', handleMove);
-    }
+      document.addEventListener('mousemove', throttle(handleMove, 100));
+    },
     // 2. 종속성 설정
     //    가급적 종속성 설정은 필요
     //    설정하지 않은 경우 문제가 발생 (예: 성능)
-    // []
+    // 마운트 시점 이후 1회 실행
+    []
   );
 
   return (
--- 15-effect-sync-external-system/src/utils/index.js
+++ 15-effect-sync-external-system/src/utils/index.js
@@ -1,1 +1,3 @@
+export * from './debounce';
+export * from './throttle';
 export * from './web-storage';
