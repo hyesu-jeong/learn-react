30f3ae91e06a8ac94c2c8b46ea590a68f9c7758b
hyesu-jeong <hyeppy739@gmail.com>
2024-08-19 14:12:08 +0900

    [학습] 싱글 페이지 앱 with React Router 7
    - 중첩된 루트(Nested routes)
    - 중첩된 레이아웃 (NotesLayout)

--- 16-using-custom-hooks/src/router.jsx
+++ 16-using-custom-hooks/src/router.jsx
@@ -1,42 +1,18 @@
 import { createBrowserRouter } from 'react-router-dom';
 
 import HomePage from '@/pages/Home';
+import RootLayout from '@/pages/layout/RootLayout';
+
+import NotesLayout from '@/pages/layout/NotesLayout';
 import NewNotePage from '@/pages/NewNote';
 import NoteDetailPage from '@/pages/NoteDetail';
 import NoteListPage from '@/pages/NoteList';
-import RootLayout from '@/pages/layout/RootLayout';
-
-// Legacy React Router v6.3-
-// const routesFromElement = createRoutesFromElements(
-//   <>
-//     <Route path="/" element={<HomePage />} />
-//     <Route path="/notes" element={<NoteListPage />} />
-//     <Route path="/notes/new" element={<NewNotePage />} />
-//     <Route path="/notes/detail" element={<NoteDetailPage />} />
-//   </>
-// );
-
-// const router = createBrowserRouter(routesFromElement);
 
 // React Router v6.4+ (data APIs ✅)
-// /
-//   notes    -> /notes
-//     new    -> /notes/new
-//     detail -> /notes/detail
-//       edit -> /notes/detail/edit
-//
-//   admin     -> /admin
-//     chart   -> /admin/chart
-//     survey  -> /admin/survey
-//     summary -> /admin/summary
 const routes = [
-  // Root Layout (Parent)
   {
     path: '/',
     element: <RootLayout />,
-
-    // Nested Routes
-    // Children components
     children: [
       {
         index: true,
@@ -44,15 +20,21 @@ const routes = [
       },
       {
         path: 'notes',
-        element: <NoteListPage />,
-      },
-      {
-        path: 'notes/new',
-        element: <NewNotePage />,
-      },
-      {
-        path: 'notes/detail',
-        element: <NoteDetailPage />,
+        element: <NotesLayout />,
+        children: [
+          {
+            index: true,
+            element: <NoteListPage />,
+          },
+          {
+            path: 'new',
+            element: <NewNotePage />,
+          },
+          {
+            path: 'detail',
+            element: <NoteDetailPage />,
+          },
+        ],
       },
     ],
   },
