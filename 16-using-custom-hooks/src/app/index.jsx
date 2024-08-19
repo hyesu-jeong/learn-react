d5e7919e57a563345ca28dfde2eafc541340fbcf
hyesu-jeong <hyeppy739@gmail.com>
2024-08-19 11:43:39 +0900

    [학습] 싱글 페이지 앱 with React Router
    - react-router-dom 패키지 설치
    - router 생성 (createBrowserRouter() 함수)
    - <RouterProvider router={router} />

---
+++ 16-using-custom-hooks/src/app/index.jsx
@@ -0,0 +1,38 @@
+import HomePage from '@/pages/Home';
+import NewNotePage from '@/pages/NewNote';
+import NoteDetailPage from '@/pages/NoteDetail';
+import NoteListPage from '@/pages/NoteList';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+const routes = [
+  // Route object
+  // { path?: string, element?: React.ReactNode | null }
+  {
+    path: '/',
+    element: <HomePage />,
+  },
+  {
+    path: '/notes',
+    element: <NoteListPage />,
+  },
+  {
+    path: '/notes/new',
+    element: <NewNotePage />,
+  },
+  {
+    path: '/notes/detail',
+    element: <NoteDetailPage />,
+  },
+];
+
+const router = createBrowserRouter(routes);
+
+function App() {
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
