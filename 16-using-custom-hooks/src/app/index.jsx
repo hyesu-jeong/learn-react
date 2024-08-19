7cfa45d6c2ce09ca0dd1a585c3f2cef96c0187c6
hyesu-jeong <hyeppy739@gmail.com>
2024-08-19 13:21:39 +0900

    [학습] 싱글 페이지 앱 with React Router 4
    - createRoutesFromElements() 함수 활용
    - 레가시 리액트 라우터 API 활용

--- 16-using-custom-hooks/src/app/index.jsx
+++ 16-using-custom-hooks/src/app/index.jsx
@@ -1,34 +1,5 @@
-import HomePage from '@/pages/Home';
-import NewNotePage from '@/pages/NewNote';
-import NoteDetailPage from '@/pages/NoteDetail';
-import NoteListPage from '@/pages/NoteList';
-import {
-  createBrowserRouter as createRouter,
-  RouterProvider,
-} from 'react-router-dom';
-
-const routes = [
-  // Route object
-  // { path?: string, element?: React.ReactNode | null }
-  {
-    path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/notes',
-    element: <NoteListPage />,
-  },
-  {
-    path: '/notes/new',
-    element: <NewNotePage />,
-  },
-  {
-    path: '/notes/detail',
-    element: <NoteDetailPage />,
-  },
-];
-
-const router = createRouter(routes);
+import router from '@/router';
+import { RouterProvider } from 'react-router-dom';
 
 function App() {
   return (
