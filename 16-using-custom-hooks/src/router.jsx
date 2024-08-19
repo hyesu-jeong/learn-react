06069902672c4ec02a454723883623ac99f74ec6
hyesu-jeong <hyeppy739@gmail.com>
2024-08-19 14:03:15 +0900

    [학습] 싱글 페이지 앱 with React Router 6
    - <RootLayout> 컴포넌트
    - 글로벌 내비게이션 구성

--- 16-using-custom-hooks/src/router.jsx
+++ 16-using-custom-hooks/src/router.jsx
@@ -1,47 +1,63 @@
-/* eslint-disable no-unused-vars */
-import {
-  createBrowserRouter,
-  createRoutesFromElements,
-  Route,
-} from 'react-router-dom';
+import { createBrowserRouter } from 'react-router-dom';
 
 import HomePage from '@/pages/Home';
 import NewNotePage from '@/pages/NewNote';
 import NoteDetailPage from '@/pages/NoteDetail';
 import NoteListPage from '@/pages/NoteList';
+import RootLayout from '@/pages/layout/RootLayout';
+
+// Legacy React Router v6.3-
+// const routesFromElement = createRoutesFromElements(
+//   <>
+//     <Route path="/" element={<HomePage />} />
+//     <Route path="/notes" element={<NoteListPage />} />
+//     <Route path="/notes/new" element={<NewNotePage />} />
+//     <Route path="/notes/detail" element={<NoteDetailPage />} />
+//   </>
+// );
+
+// const router = createBrowserRouter(routesFromElement);
 
 // React Router v6.4+ (data APIs ✅)
+// /
+//   notes    -> /notes
+//     new    -> /notes/new
+//     detail -> /notes/detail
+//       edit -> /notes/detail/edit
+//
+//   admin     -> /admin
+//     chart   -> /admin/chart
+//     survey  -> /admin/survey
+//     summary -> /admin/summary
 const routes = [
+  // Root Layout (Parent)
   {
     path: '/',
-    element: <HomePage />,
-  },
-  {
-    path: '/notes',
-    element: <NoteListPage />,
-  },
-  {
-    path: '/notes/new',
-    element: <NewNotePage />,
-  },
-  {
-    path: '/notes/detail',
-    element: <NoteDetailPage />,
+    element: <RootLayout />,
+
+    // Nested Routes
+    // Children components
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'notes',
+        element: <NoteListPage />,
+      },
+      {
+        path: 'notes/new',
+        element: <NewNotePage />,
+      },
+      {
+        path: 'notes/detail',
+        element: <NoteDetailPage />,
+      },
+    ],
   },
 ];
 
-// const router = createBrowserRouter(routes);
-
-// Legacy React Router v6.3-
-const routesFromElement = createRoutesFromElements(
-  <>
-    <Route path="/" element={<HomePage />} />
-    <Route path="/notes" element={<NoteListPage />} />
-    <Route path="/notes/new" element={<NewNotePage />} />
-    <Route path="/notes/detail" element={<NoteDetailPage />} />
-  </>
-);
-
-const router = createBrowserRouter(routesFromElement);
+const router = createBrowserRouter(routes);
 
 export default router;
