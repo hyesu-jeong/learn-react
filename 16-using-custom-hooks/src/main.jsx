3bb46849bebca33b6d1f507f927da3e06f2b6bdc
hyesu-jeong <hyeppy739@gmail.com>
2024-08-19 15:03:12 +0900

    [학습] 싱글 페이지 앱 with React Router 8
    - 내비게이션 링크 설정
    - <NavLink> 컴포넌트
    - 활성 클래스 이름 설정

--- 16-using-custom-hooks/src/main.jsx
+++ 16-using-custom-hooks/src/main.jsx
@@ -1,7 +1,7 @@
+import App from '@/app';
 import '@/styles/main.css';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './app';
 
 const container = document.getElementById('react-app');
 
