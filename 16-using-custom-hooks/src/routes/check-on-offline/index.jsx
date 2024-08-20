a4b3e3c68f16cad7a8114f8645ed13adc3c2631c
hyesu-jeong <hyeppy739@gmail.com>
2024-08-20 13:03:00 +0900

    [학습] 커스텀 훅 8 - useToggle() 훅

--- 16-using-custom-hooks/src/routes/check-on-offline/index.jsx
+++ 16-using-custom-hooks/src/routes/check-on-offline/index.jsx
@@ -1,15 +1,20 @@
-import useOnline from '@/hooks/useOnline';
-import useStateWithCallback from '@/hooks/useStateWithCallback';
 import { useRef } from 'react';
+// import useOnline from '@/hooks/useOnline';
+import useStateWithCallback from '@/hooks/useStateWithCallback';
+import useToggle from '@/hooks/useToggle';
 import Switcher from '../sync-web-storage/components/Switcher';
 
 function CheckOnOffline() {
-  const isOnline = useOnline();
+  // const isOnline = useOnline();
+  const [isToggle, setIsToggle] = useToggle(false, (nextIsToggle) => {
+    document.body.style.backgroundColor = nextIsToggle ? '#2f2f2f' : 'white';
+  });
 
   return (
     <div style={{ display: 'flex', flexFlow: 'column', gap: 20 }}>
       <h1>Check On/Offline</h1>
-      <Switcher value={isOnline} />
+      <Switcher value={isToggle} onToggle={() => setIsToggle((t) => !t)} />
+      <p>{isToggle.toString()}</p>
 
       <ChangeStateAndCallback />
     </div>
