53f7d42e1ae53906e1c2addf2a7fa1fc6dd273bc
hyesu-jeong <hyeppy739@gmail.com>
2024-08-22 13:13:27 +0900

    [학습] 리듀서를 사용한 상태 관리 4 - 좀 더 복잡한 상태 관리 - 리스트를 처리하는 기능을 useReducer 훅으로 관리

--- 17-advanced-state-management/src/routes/task-manager/index.jsx
+++ 17-advanced-state-management/src/routes/task-manager/index.jsx
@@ -2,6 +2,7 @@ import { AppDivider, AppLink } from '@/components';
 import useDocumentTitle from '@/hooks/useDocumentTitle';
 import guideImage from './assets/task-manager-guide.png';
 import AuthStatus from './components/AuthStatus';
+import ListManager from './components/ListManager';
 import TaskManager from './components/TaskManager';
 import S from './style.module.css';
 
@@ -11,6 +12,10 @@ function TaskManagerUsingReducer() {
   return (
     <main id="page" className={S.component}>
       <h1 className="headline">태스크 매니저 (리듀서 활용)</h1>
+
+      <ListManager />
+
+      <AppDivider />
 
       <AuthStatus />
 
